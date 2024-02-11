2f68da54b36ccbb4a857d6f65786e83cc3b364ee
Nirjal Shakya <nirjalshakya17@gmail.com>
2024-02-11 15:42:16 -0800

    Updated footer design

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { router } from "./constant/routes.js";
+import { companyInfo } from "./constant/companyInfo.js";
 import { createPinia } from 'pinia';
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia).use(router);
 app.mount("#app");
 export { router };
+export { companyInfo };
