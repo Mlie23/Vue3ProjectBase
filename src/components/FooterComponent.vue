91214fd18b7add923dc51e2e2b7a7ceb453dc002
Nirjal Shakya <nirjalshakya17@gmail.com>
2024-03-04 16:00:19 -0800

    Ran yarn fix

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,13 +1,27 @@
 <template>
   <footer class="bg-white shadow dark:bg-gray-800">
     <div class="w-full p-4 flex justify-center items-center">
-      <div class="grid gap-4 grid-cols-2 place-content-between lg:place-items-center lg:grid-cols-3">
-        <div class="text-sm text-gray-300 place-self-start lg:place-self-center">{{ companyInfo.phoneNum }}</div>
-        <div class="text-sm text-gray-300 place-self-end lg:place-self-center">{{ companyInfo.address }}</div>
-        <div class="text-sm text-gray-300 place-self-start lg:place-self-center">
+      <div
+        class="grid gap-4 grid-cols-2 place-content-between lg:place-items-center lg:grid-cols-3"
+      >
+        <div
+          class="text-sm text-gray-300 place-self-start lg:place-self-center"
+        >
+          {{ companyInfo.phoneNum }}
+        </div>
+        <div class="text-sm text-gray-300 place-self-end lg:place-self-center">
+          {{ companyInfo.address }}
+        </div>
+        <div
+          class="text-sm text-gray-300 place-self-start lg:place-self-center"
+        >
           <a :href="mailTo"> {{ companyInfo.email }} </a>
         </div>
-        <div class="text-sm text-gray-400 col-span-1 place-self-end lg:col-span-3 lg:place-self-center">{{ companyInfo.name }} © 2024</div>
+        <div
+          class="text-sm text-gray-400 col-span-1 place-self-end lg:col-span-3 lg:place-self-center"
+        >
+          {{ companyInfo.name }} © 2024
+        </div>
       </div>
     </div>
   </footer>
